1e36bfa545fb30077fcd4381c08528b63efcb805
abderehmen02 <ilahalaha0987@gmail.com>
2022-07-27 22:53:50 +0200

    first commit

---
+++ Front End/src/index.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react'
+import reactDom from 'react-dom'
+
+const App:React.FC = () =>{
+    return (
+        <div>
+<div>hello typescript</div>
+        </div>
+    )
+}
+reactDom.render( <App/> , document.getElementById('root') )

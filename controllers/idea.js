3acbba284ed2d8c341881eb5da247701da9088e1
abderehmen02 <ilahalaha0987@gmail.com>
2022-08-03 18:58:43 +0200

    ideas api

--- controllers/idea.js
+++ controllers/idea.js
@@ -2,13 +2,11 @@ const ideaModel = require("../db/models/newIdea")
 const asyncWrapper = require("../helpers/asyncWrapper")
 const {StatusCodes} = require('http-status-codes')
 const {sendError } =  require("../helpers/sendError")
+const {validateIdea} = require('../validators/index')
 const createNewIdea =asyncWrapper(  async (req , res)=>{
-if(!req.body.text) { sendError(res , StatusCodes.BAD_REQUEST  , 'text does not exist' )  } 
-console.log(req.user._id)
-console.log("user req")
-console.log(req.user)
-console.log(res.cookies.day-afer-day)
-const newUser = await   ideaModel.create({ ...req.body , user : req.user._id })
+// if(!req.body.text) { sendError(res , StatusCodes.BAD_REQUEST  , 'text does not exist' )  } 
+const {value : ideaObj} = validateIdea(req.body)   ; 
+const newUser = await   ideaModel.create({ ...ideaObj , user : req.user._doc._id })
 res.status(201).json(newUser)
 } )
 module.exports = {createNewIdea}

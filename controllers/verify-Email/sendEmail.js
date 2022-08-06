d9db9293813f987805fda6d98ff2e4e3d85c5ea5
abderehmen02 <ilahalaha0987@gmail.com>
2022-08-06 23:59:30 +0200

    sending email

---
+++ controllers/verify-Email/sendEmail.js
@@ -0,0 +1,43 @@
+const nodemailer = require('nodemailer');
+const {EMAIL , EMAIL_PASSWORD  ,URL , EMAIL_TOKEN_SECRET } = require("../../config/default")
+const jwt = require("jsonwebtoken")
+const {sendError } = require("../../helpers/sendError")
+const successStatus = require("../../helpers/successStatus")
+const asyncWraper = require("../../helpers/asyncWrapper")
+const sendMail = asyncWraper( async (receiver)=>{
+const token = jwt.sign(EMAIL_TOKEN_SECRET , receiver)  ; 
+const link = `${URL}/api/confirmEmail/^${token}`
+// setting the html to send
+const html =  `
+<h3> welcome to Day-After-Day </h3>
+<ph3>please confirm your email by clicking this link${link}</p>
+<p> our app will help you managae your day and increase your productivity </p>
+` 
+    
+let transporter = nodemailer.createTransport({
+    host: 'mail.YOURDOMAIN.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user:  EMAIL , 
+        pass:  EMAIL_PASSWORD 
+    },
+    tls:{
+      rejectUnauthorized:false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+      from: 'DAY-AFTER-DAY', // sender address
+      to: receiver.email, // list of receivers
+      subject: 'Confirm Your Account', // Subject line
+      text: `Hello ${receiver.fullName}`, // plain text body
+      html 
+  };
+
+  // send mail with defined transport object
+ const result = await  transporter.sendMail(mailOptions);
+  return result 
+})
+module.exports = sendMail 
---
+++ q
@@ -0,0 +1,7 @@
+  dailing[m
+  email-ver[m
+  goal[m
+  ideas[m
+  joi[m
+* [32mmaster[m
+  prod[m

74a7f1cdeb1fd6f2c5613241b6f1ba29c9a5c9d1
abderehmen02 <ilahalaha0987@gmail.com>
2022-08-06 23:03:09 +0200

    setting email verification

--- config/default.js
+++ config/default.js
@@ -3,5 +3,8 @@ module.exports = {
     PORT :  process.env.PORT || 2000,
     URL : process.env.URL ,
     MONGO_DB_CONNECT  : process.env.MONGO_DB_CONNECT   , 
-    WEB_TOKEN_SECRET : process.env.WEB_TOKEN_SECRET  
+    WEB_TOKEN_SECRET : process.env.WEB_TOKEN_SECRET   ,
+    EMAIL_TOKEN_SECRET : process.env.EMAIL_TOKEN_SECRET  ,
+    EMAIL : process.env.EMAIL , 
+    EMAIL_PASSWORD : process.env.EMAIL_PASSWORD
 }

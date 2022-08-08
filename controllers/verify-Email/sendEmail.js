6670f64108bc7cd388121fd8c08a59bb73eb3f4e
abderehmen02 <ilahalaha0987@gmail.com>
2022-08-08 20:50:13 +0200

    send email

--- controllers/verify-Email/sendEmail.js
+++ controllers/verify-Email/sendEmail.js
@@ -3,6 +3,7 @@ const {EMAIL , EMAIL_PASSWORD  ,URL , EMAIL_TOKEN_SECRET } = require("../../conf
 const jwt = require("jsonwebtoken")
 const asyncWraper = require("../../helpers/asyncWrapper")
 const sendMail = asyncWraper( async (receiver)=>{
+  console.log("send email function")
 const token = jwt.sign(receiver , EMAIL_TOKEN_SECRET)  ; 
 const link = `${URL}/api/confirmEmail/^${token}`
 // setting the html to send
@@ -11,32 +12,35 @@ const html =  `
 <ph3>please confirm your email by clicking this link${link}</p>
 <p> our app will help you managae your day and increase your productivity </p>
 ` 
-    
+    console.log("token link")
+    console.log(token , link)
+    console.log(EMAIL)
+    console.log("email")
+    console.log(EMAIL_PASSWORD)
 let transporter = nodemailer.createTransport({
-    host: 'mail.YOURDOMAIN.com',
-    port: 587,
-    secure: false,
+  service : "gmail"  , 
     auth: {
         user:  EMAIL , 
         pass:  EMAIL_PASSWORD 
-    },
-    tls:{
-      rejectUnauthorized:false
     }
   });
-
+console.log("transporter")
+console.log(transporter)
   // setup email data with unicode symbols
-  let mailOptions = {
-      from: 'DAY-AFTER-DAY', // sender address
-      to: receiver.email, // list of receivers
+let mailOptions = {
+      from: EMAIL, // sender address
+      to: 'ilahalaha0987@gmail.com', // list of receivers
       subject: 'Confirm Your Account', // Subject line
       text: `Hello ${receiver.fullName}`, // plain text body
       html 
   };
-
-  // send mail with defined transport object
-  console.log("sending email")
- const result = await  transporter.sendMail(mailOptions);
+console.log("mailoptions")
+console.log(mailOptions)
+// send mail with defined transport object
+console.log("sending email")
+const result = await transporter.sendMail(mailOptions);
+console.log("result")
+console.log(result)
   return result 
 })
 module.exports = sendMail 

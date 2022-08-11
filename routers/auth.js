df113000528d011c7d7cb5b910b91cd89f32733a
abderehmen02 <ilahalaha0987@gmail.com>
2022-08-11 15:49:17 +0200

    run the server

--- routers/auth.js
+++ routers/auth.js
@@ -1,12 +1,11 @@
 const express  = require("express")
 const router = express.Router() ; 
 const { regester, login } = require("../controllers/auth")
-const {sendEmail} = require("../controllers/verify-Email")
 
 
 
 // setting the authontification router
-router.post("/regester"   , sendEmail ,  regester )
+router.post("/regester"   ,  regester )
 router.post("/login"  , login)
 
 

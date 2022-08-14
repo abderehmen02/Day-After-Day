9ddc514f4e22cfaf2a6c3c43f7ef48a146f3fc5b
abderehmen02 <ilahalaha0987@gmail.com>
2022-08-14 11:54:55 +0200

    run the server

--- config/default.js
+++ config/default.js
@@ -1,8 +1,8 @@
 module.exports = {
     PORT :  process.env.PORT || 2000,
     URL : process.env.URL ,
-    MONGO_DB_CONNECT  : process.env.MONGO_DB_CONNECT   , 
-    WEB_TOKEN_SECRET : process.env.WEB_TOKEN_SECRET   ,
+    MONGO_DB_CONNECT  : 'mongodb+srv://abderehmen02:abdo2015abdo2015@cluster0.xi3z5.mongodb.net/?retryWrites=true&w=majority'   , 
+    WEB_TOKEN_SECRET : 'b6f35752ebef1e6684cb418520c656afaab438ffa08476c7c492d3030c5d792637c2616042c5a2da162dca1e613baafedc0c06399e1dd735e038c1874dffcba2'   ,
     EMAIL_TOKEN_SECRET : process.env.EMAIL_TOKEN_SECRET  ,
     EMAIL : process.env.EMAIL , 
     EMAIL_PASSWORD : process.env.EMAIL_PASSWORD

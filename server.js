d3290a4cf5354073606451f31f6901a022a3e6a2
abderehmen02 <ilahalaha0987@gmail.com>
2022-08-14 10:27:21 +0200

    run the server

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ const dailingRouter = require("./routers/dailing")
 
 // setting the middlewares
 app.use(cors())
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(express.json())
 app.get("/"  , (req , res)=>{
     res.send("hello")

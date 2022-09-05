81b31bcf05448bf1303db385cf7c37a52d673a20
abderehmen02 <ilahalaha0987@gmail.com>
2022-09-05 14:33:37 +0200

    first app complete

--- Front End/src/features/home/conmponents.tsx
+++ Front End/src/features/home/conmponents.tsx
@@ -1,42 +1,41 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
 import './index.css'
-// import {motion} from 'framer-motion'
-// import TypeWriterEffect from 'react-typewriter-effect';
-import { Link } from 'react-router-dom'
-// initial={{rotateZ : 0}} animate={{rotateZ : [6 , -6 , 5 , -5 , 2 , -2 , 0]}} transition={{duration : 0.5 ,  }}
+import { Link, useNavigate } from 'react-router-dom'
+import {login as loginAction} from '../../features/login'
+import * as actionCreators from '../../state/actionCreators'
+import { useDispatch } from 'react-redux'
+import { Navigate } from 'react-router-dom'
+
+
 export const  Header = (): JSX.Element =>{
     const titleInfoText = "Where you can increase your productivity and acheive your goals faster"
   return (
   <div className="header">
     <div className='homeTitle'  > Welcome To Day After Day <div className='logoHome' ></div> </div>
-{/* <TypeWriterEffect
-        textStyle={{
-          fontFamily: 'Red Hat Display',
-          color: '#3F3D56',
-          fontWeight: 500,
-          fontSize: '1.5em',
-        }}
-        startDelay={2000}
-        cursorColor="#3F3D56"
-        multiText={[
-          'Hey there, This is a type writer animation package',
-          'it consist of two types...',
-          'Single text display and multi text display',
-          'Fonts can be customized.',
-          'The type speed can be customized as well',
-        ]}
-        multiTextDelay={1000}
-        typeSpeed={30}
-      />  */}  </div> 
+  </div> 
   
   )
 }
 
+
+
 // initial={{x : -50 ,y : 50}} animate={{x : 0 , y: 0}} transition={{duration : 0.5}}
 export const Buttons = () : JSX.Element =>{
+   const dispatch = useDispatch()
+   const navigate = useNavigate()
+   const {login , emitAction } = bindActionCreators(actionCreators , dispatch)
+
+    const loginGuest = ()=>{
+      console.log(process.env.REACT_APP_GUEST_USERNAME)
+      console.log(process.env.REACT_APP_GUEST_PASSWORD)
+      console.log("data")
+     loginAction({ email  : process.env.REACT_APP_GUEST_USERNAME , password :  process.env.REACT_APP_GUEST_PASSWORD } , login , emitAction , navigate )
+    }
     return           <div className="homeButtons"  >
     <button className='homeBtn loginBtn'    ><Link  className='link'  to='/login'  > login <i className="bi bi-box-arrow-in-right"></i> </Link> <br/></button>
     <button className='homeBtn regesterBtn' ><Link  className='link'  to='/regester' > regester <i className="bi bi-person-plus icon"></i></Link></button>
     <button className='homeBtn aboutHomeBtn' ><Link className='link' to="/about" > about the app <i className="bi bi-journal-text"></i> </Link> </button>
+    <button className='homeBtn loginGuestBtn' onClick={loginGuest} > login as a guest </button>
     </div>
 }
--- Front End/src/pages/home.tsx
+++ Front End/src/pages/home.tsx
@@ -4,9 +4,12 @@ import '../features/home/index.css'
 import { Buttons, Header } from '../features/home/conmponents'
 import homeImgOne from '../assets/images/goal6.png'
 import homeImgTwo from  '../assets/images/goal11.png'
+import * as actionCreators from '../state/actionCreators'
+import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 export const Home: React.FC =  ()=> {
-
+   
   return (
     <div className='homePage' >
       <Header/>

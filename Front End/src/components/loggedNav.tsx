970949d33f1695a66ee9ff55b9d802d105824ed0
abderehmen02 <ilahalaha0987@gmail.com>
2022-09-11 13:48:43 +0200

    setting the nav component more button

--- Front End/src/components/loggedNav.tsx
+++ Front End/src/components/loggedNav.tsx
@@ -10,18 +10,18 @@ function NavBar() {
     <div>
       <LogOut/>
     </div>
-    <div className='item'>
+    <div className='item hoverEfect'>
       <Link to="/productivity" className='link'> Productivity </Link>
     </div>
-    <div className='item'>
-      <Link to="/goal"  className='link' > Goals </Link>
+    <div className='item hoverEfect'>
+      <Link to="/goal "  className='link' > Goals </Link>
     </div>
     <div className='item moreBtn' >
-{  DisplayList ||     <button onClick={()=>{setDisplayList(true)}} > more</button> }
+{  DisplayList ?  <i className="bi bi-x-circle closeIcon icon" onClick={()=>{setDisplayList(false)}} ></i> :   <i className="bi bi-three-dots icon"   onClick={()=>{setDisplayList(true)}}></i>    }
       {
         DisplayList && <div className='list' >
-          <div > <Link to="/about" className='link' > About </Link> </div>
-          <div> <Link  to="/journal"  className='link' > Journal </Link> </div>
+          <div className='listItemMoreNav hoverEfect' > <Link to="/about" className='link' > About </Link> </div>
+          <div  className='listItemMoreNav hoverEfect ' > <Link  to="/journal"  className='link' > Journal </Link> </div>
           </div>
       }
       </div>

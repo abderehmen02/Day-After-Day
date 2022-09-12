4c0d6916bf426307aee1a58ee12aacd139aaa9f1
abderehmen02 <ilahalaha0987@gmail.com>
2022-09-12 23:00:38 +0200

    delete model

--- controllers/events.js
+++ controllers/events.js
@@ -1,14 +1,21 @@
 import { StatusCodes } from "http-status-codes";
 import { sendErr } from "../helpers/sendError";
 import successStatus from "../helpers/successStatus";
+import { validateEvent } from "../validators";
 
 const asyncWrapper  = require("../helpers/asyncWrapper") ; 
 const eventModel = require('../db/models/events.js')
 
-export const CreateModel = asyncWrapper(async (req , res)=>{
-const {value : event} = valid
+export const createModel = asyncWrapper(async (req , res)=>{
+const {value : event} = validateEvent
 const model = await eventModel.create({...event , user : req.user }) ; 
 if(model) return successStatus(res  , StatusCodes.CREATED , model ) ; 
 sendErr(res , StatusCodes.INTERNAL_SERVER_ERROR , 'can not create model' ) ; 
+})
 
+export const deleteModel = asyncWrapper(async (req  , res)=>{
+    const model = await eventModel.findOneAndDelete({user : req.user._id , _id : req.params._id}) ; 
+    if(model) { 
+        const models = await eventModel.find({user : req.user._id})
+        successStatus(res , StatusCodes.OK , ) }
 })

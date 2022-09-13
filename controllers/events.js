ed75d3e6cc616f2d3e38b66ef2225d249ec93785
abderehmen02 <ilahalaha0987@gmail.com>
2022-09-13 11:15:49 +0200

    testing the create event api

--- controllers/events.js
+++ controllers/events.js
@@ -1,19 +1,22 @@
-import { StatusCodes } from "http-status-codes";
-import { sendErr } from "../helpers/sendError";
-import successStatus from "../helpers/successStatus";
-import { validateEvent } from "../validators";
+const { StatusCodes } = require( "http-status-codes");
+const { sendErr } = require("../helpers/sendError");
+const successStatus = require("../helpers/successStatus");
+const { validateEvent } = require("../validators");
 
 const asyncWrapper  = require("../helpers/asyncWrapper") ; 
-const eventModel = require('../db/models/events')
+const eventModel = require('../db/models/events')  ;
 
-export const createEvent = asyncWrapper(async (req , res)=>{
-const {value : event} = validateEvent
-const model = await eventModel.create({...event , user : req.user }) ; 
-if(model) return successStatus(res  , StatusCodes.CREATED , model ) ; 
+ const createEvent = asyncWrapper(async (req , res)=>{
+console.log("creave event ")  ;
+const {value : event} = validateEvent(req.body) ;  
+const model = await eventModel.create({...event , user : req.user._id }) ; 
+if(model){ 
+    const models = await eventModel.find({user : req.user._id}) ;
+    return successStatus(res  , StatusCodes.CREATED , models ) }; 
 sendErr(res , StatusCodes.INTERNAL_SERVER_ERROR , 'can not create model' ) ; 
 })
 
-export const deleteEvent = asyncWrapper(async (req  , res)=>{
+ const deleteEvent = asyncWrapper(async (req  , res)=>{
     const model = await eventModel.findOneAndDelete({user : req.user._id , _id : req.params._id}) ; 
     if(model) { 
         const models = await eventModel.find({user : req.user._id})
@@ -21,7 +24,7 @@ export const deleteEvent = asyncWrapper(async (req  , res)=>{
     sendErr(res , StatusCodes.INTERNAL_SERVER_ERROR , 'can not delete model')
 })
 
-export const editEvent = asyncWrapper(async (req , res)=>{
+ const editEvent = asyncWrapper(async (req , res)=>{
     const {value : event} = validateEvent(req.body)
   const updatedEvent = await eventModel.findOneAndDelete({user : req.user._id , _id : req.params.id } , {...event} , {new : true})  
  if(updatedEvent){
@@ -32,8 +35,10 @@ sendErr(res , StatusCodes.INTERNAL_SERVER_ERROR , 'can not edit that event')
 }) 
 
 
-export const getEvents = asyncWrapper(async (req , res)=>{
+ const getEvents = asyncWrapper(async (req , res)=>{
     const allEvents = await eventModel.find({user : req.user._id}) ;
     if(allEvents) return successStatus(res , StatusCodes.OK , allEvents) ; 
     sendErr(res  ,StatusCodes.INTERNAL_SERVER_ERROR , 'can not get the events for that user')
 })
+
+module.exports = {createEvent , deleteEvent , getEvents , editEvent}
--- routers/events.js
+++ routers/events.js
@@ -1,4 +1,4 @@
-const router = require("express").Router ; 
+const router = require("express").Router() ; 
 const { getEvents, createEvent, deleteEvent } = require("../controllers/events");
 const authorize = require("../middleWares/authorization")
 

133a1814f93198778b04ccb2e4acc82202d271a5
abderehmen02 <ilahalaha0987@gmail.com>
2022-09-14 12:37:58 +0200

    setting the event state types

---
+++ Front End/src/types/state/event.ts
@@ -0,0 +1,40 @@
+import { type } from "os"
+
+export interface oneEvent {
+    title : string , 
+    descreption  : string , 
+    date : string 
+}
+export interface eventState{
+    data : oneEvent[] ,
+    loading : boolean , 
+    error : any
+}
+export enum eventActions {
+   EVENT_REQUEST = 'EVENT_REQUEST' ,
+   EVENT_ERROR = 'EVENT_ERROR' ,
+   EVENT_SUCCUSS = 'EVENT_SUCCUSS' ,
+   EVENT_RESET = 'EVENT_RESET'
+}
+
+
+export interface eventSuccussAction {
+    type : eventActions.EVENT_SUCCUSS ,
+    payload : oneEvent[] ,
+}
+
+
+export interface eventErrorAction {
+    type :  eventActions.EVENT_ERROR ,
+    payload : any 
+}
+
+export interface eventRequestAction {
+    type : eventActions.EVENT_REQUEST ,
+}
+export interface eventResetAction {
+    type : eventActions.EVENT_RESET
+}
+
+
+export type eventAction = eventErrorAction | eventRequestAction | eventResetAction | eventSuccussAction

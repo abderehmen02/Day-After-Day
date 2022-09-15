147fb6933b364bdcc54ce9071a2763385f44132c
abderehmen02 <ilahalaha0987@gmail.com>
2022-09-15 14:28:49 +0200

    setting the create event

--- Front End/src/features/events/components.tsx
+++ Front End/src/features/events/components.tsx
@@ -1,18 +1,19 @@
-import React  , { useEffect }from 'react'
+import React  , { useEffect , useState }from 'react'
 import {  useDispatch, useSelector } from 'react-redux'
 import { stateType } from '../../state/reducers'
 import { eventState  , userLoginState} from '../../types'
 import { getEvents } from './functions'
 import * as actionCreators from '../../state/actionCreators'
 import { Dispatch  , bindActionCreators } from 'redux'
-export function MapingEvents() {
+export function MapingEvents(): JSX.Element {
 
 const   event : eventState  = useSelector((state: stateType)=>state.event)
 const   dispatch : Dispatch= useDispatch() ; 
 const   { emitAction } = bindActionCreators(actionCreators , dispatch)
 const    userLogin : userLoginState = useSelector((state :  stateType)=>state.userLogin)
-console.log("event") ; 
-console.log(event)
+
+
+
 useEffect(() => {
     if(userLogin.token){
         getEvents(userLogin.token , emitAction ) ; 
@@ -20,7 +21,22 @@ useEffect(() => {
 }, [userLogin])
 
   return (
-    <div>eventss</div>
+    <div>eventss
+    </div>
   )
 }
 
+
+export function CreateEvent():JSX.Element{
+    const [title, setTitle] = useState<string | undefined >("")        ;
+     const [descreption, setDescreption] = useState<string | undefined>("")  ;
+     const [date, setDate] = useState<Date | undefined >(new Date())
+     return (
+  <div>
+ <input  value={title} onChange={e=>setTitle(e.target.value)} ></input>
+ <input   value={descreption}  onChange={e =>setDescreption(e.target.value) } ></input>
+ <input   value={date}    onChange={e=>setDate(e.target.value)} ></input>
+ <button></button>
+ </div>   
+ )
+}

a418ffc897e1f5f720082fa0dc46c33467b92db2
abderehmen02 <ilahalaha0987@gmail.com>
2022-09-15 12:50:13 +0200

    saving the api

--- Front End/src/pages/event.tsx
+++ Front End/src/pages/event.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import {useDispatch , useSelector} from 'react-redux'
+import { stateType } from '../state/reducers'
+
 
 function Event() {
+  const state : stateType = useSelector((state: stateType)=>state )
+  console.log(state)
   return (
     <div>THIS IS THE EVENT PAGE</div>
   )

6a3ce1c99376922c00b5a3c0ba8af59091e2a863
abderehmen02 <ilahalaha0987@gmail.com>
2023-02-04 17:51:57 +0100

    running the server

---
+++ build/precache-manifest.8e09c8f1620f19b61c0bb87fef7ca0ca.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fee389c7e212d88c027e7d085ff7c02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db69aaba6eed5eb465d0",
+    "url": "/static/css/2.a9562147.chunk.css"
+  },
+  {
+    "revision": "5279441105f86ab215e9",
+    "url": "/static/css/main.3bc6dfb3.chunk.css"
+  },
+  {
+    "revision": "db69aaba6eed5eb465d0",
+    "url": "/static/js/2.d2df1713.chunk.js"
+  },
+  {
+    "revision": "cd576662df45a4474113d414b73d3940",
+    "url": "/static/js/2.d2df1713.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5279441105f86ab215e9",
+    "url": "/static/js/main.75147ad3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.75147ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "680767c32f07ea455ebf",
+    "url": "/static/js/runtime-main.bad5f64f.js"
+  },
+  {
+    "revision": "52196284de1fcb5b044f001a75482dba",
+    "url": "/static/media/bootstrap-icons.52196284.woff"
+  },
+  {
+    "revision": "7f477633ddd12f84284654f2a2e89b8a",
+    "url": "/static/media/bootstrap-icons.7f477633.woff2"
+  },
+  {
+    "revision": "359b56075fc1c17a3c2de7699a6abbd3",
+    "url": "/static/media/dayAfterDayLogo.359b5607.png"
+  },
+  {
+    "revision": "7682243759a67b6938a13132b08284a5",
+    "url": "/static/media/goal2.76822437.png"
+  },
+  {
+    "revision": "1e53f8f61c1e061043a7935875c619a8",
+    "url": "/static/media/goalsVisualise.1e53f8f6.png"
+  },
+  {
+    "revision": "3080997f5a0fc0b6696b621b93739d3a",
+    "url": "/static/media/homeIllustration.3080997f.png"
+  },
+  {
+    "revision": "2192d33547029555c455f693e631e3d8",
+    "url": "/static/media/journal5.2192d335.png"
+  },
+  {
+    "revision": "ad40f4d88eac7317fbd29a0990e90e76",
+    "url": "/static/media/journal6.ad40f4d8.png"
+  },
+  {
+    "revision": "2c64c0fad94bf3dde37ee564909fa966",
+    "url": "/static/media/signUp.2c64c0fa.png"
+  }
+]);
